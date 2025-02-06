b9439faca13604d884f3d6ae52a2d775e58e44c7
dilshodismoilov <dilshodismoilov1711@gmail.com>
2025-02-06 15:05:50 +0500

    dd mm yyyy style of birth date text is now handled

--- src/birthday_text_to_number_format.py
+++ src/birthday_text_to_number_format.py
@@ -19,16 +19,31 @@ def splitStandard(birthDateText: str):
             monthText = el
     return [yearText, monthText, dayText]
 
+#case: o'n uch o'n ikki ikki ming bir (dd mm yyyy)
+def splitNoYil(birthDateText: str):
+    birthDateAr = birthDateText.split(" ")
+    dayText = " ".join(birthDateAr[:2])
+    monthText = " ".join(birthDateAr[2:4])
+    yearText = " ".join(birthDateAr[4:])
+    return [yearText, monthText, dayText]
+
 def birthDateTextToStandard(birthDateText: str):
     splitResult = splitStandard(birthDateText)
+    if len(splitResult[0]) == 0:
+        splitResult = splitNoYil(birthDateText)
     year = numberTextToInt(splitResult[0])
     if year < 100:
         year += 1900
-    month = months[splitResult[1]]
+    month = numberTextToInt(splitResult[1])
+    if month == 0:
+        month = months[splitResult[1]]
     day = numberTextToInt(splitResult[2])
-   
-    return datetime.datetime(year, month, day)
     #print(f"{year}-{month}-{day}")
+    return datetime.datetime(year, month, day)
+    
 
-#birthDateTextToStandard("ikki ming eee ... asdsad birinchi yil o'n yettinchi noyabr")
-print(birthDateTextToStandard("eee nima ediya yo'q to'qson to'qqizinchi yil birinchi iyul"))
+# birthDateTextToStandard("ikki ming eee ... asdsad birinchi yil o'n yettinchi noyabr")
+# print(birthDateTextToStandard("eee nima ediya yo'q to'qson to'qqizinchi yil birinchi iyul"))
+# print(birthDateTextToStandard("no'l uch no'l uch ikki ming olti"))
+# print(splitStandard("no'l uch no'l uch ikki ming olti"))
+# print(splitNoYil("no'l uch no'l uch ikki ming olti"))
